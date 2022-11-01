6caa926cd804c11794b941b9f5fdf0c6d939f6af
DimaKirdanov <umweltscutz@gmail.com>
2022-11-01 18:28:43 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,9 +7,19 @@ const ingredients = [
   'Condiments',
 ];
 
-const navItemElement = document.createElement('li');
-// navItemElement.textContent = ingredients.forEach((item) => { };
-navItemElement.classList.add('item');
+// Напиши скрипт, который для каждого элемента массива ingredients:
 
-const navList = document.querySelector('#ingredients');
-navList.appendChild(navItemElement);
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
+
+const ingredientsItems = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  console.log(item);
+  return item;
+});
+const ingredientsEl = document.querySelector("#ingredients");
+ingredientsEl.append(...ingredientsItems);
